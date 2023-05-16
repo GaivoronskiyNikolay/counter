15c26bb98baad2850e056ef9997a7fb6fe0127bb
GaivoronskiyNikolay <128174366+GaivoronskiyNikolay@users.noreply.github.com>
2023-05-16 15:01:27 +0300

    count

--- app/src/main/java/com/example/coun/MainActivity.java
+++ app/src/main/java/com/example/coun/MainActivity.java
@@ -14,10 +14,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    // создание полей для вывода на экран нужных значений
-    private TextView textCount; // окно вывода значения счётчика
-    private Button button; // кнопка счётчика
-    private int count = 0; // переменная счётчика
+
+    private TextView textCount;
+    private Button button;
+    private int count = 0; 
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -25,57 +25,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // присваивание переменным активити элементов представления activity_main
+
         textCount = findViewById(R.id.textCount);
         button = findViewById(R.id.button);
 
-        // выполнение действий при нажатии кнопки
+
         button.setOnClickListener(listener);
     }
 
-    // объект обработки нажатия кнопки (слушатель)
+
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            count++; // инкрементирование счётчика
+            count++;
             textCount.setText(Integer.toString(count));
         }
     };
 
     @Override
     protected void onStart() {
-        // размещаем тост (контекст, сообщение, длительность сообщения)
-        Toast toast = Toast.makeText(this, "Старт активности", Toast.LENGTH_SHORT); // инициализация
-        toast.show(); // демонстрация тоста на экране
+
+        Toast toast = Toast.makeText(this, "Старт активности", Toast.LENGTH_SHORT);
+        toast.show();
         super.onStart();
     }
 
     @Override
     protected void onStop() {
-        // размещаем тост (контекст, сообщение, длительность сообщения)
-        Toast toast = Toast.makeText(this, "Стоп активности", Toast.LENGTH_SHORT); // инициализация
-        toast.setGravity(Gravity.LEFT, 0,0); // задание позиции на экране (положение, смещение по оси Х, смещение по оси Y)
-        toast.show(); // демонстрация тоста на экране
+
+        Toast toast = Toast.makeText(this, "Стоп активности", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.LEFT, 0,0);
+        toast.show();
         super.onStop();
     }
 
     @Override
     protected void onDestroy() {
-        // размещаем тост (контекст, сообщение, длительность сообщения)
-        Toast toast = Toast.makeText(this, "Уничтожение активности", Toast.LENGTH_SHORT); // инициализация
-        toast.setGravity(Gravity.BOTTOM, 0,0); // задание позиции на экране (положение, смещение по оси Х, смещение по оси Y)
-        toast.show(); // демонстрация тоста на экране
+
+        Toast toast = Toast.makeText(this, "Уничтожение активности", Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.BOTTOM, 0,0);
+        toast.show();
         super.onDestroy();
     }
 
     @Override
     protected void onPause() {
-        // размещаем тост (контекст, сообщение, длительность сообщения)
-        Toast toast = Toast.makeText(this, R.string.pause_activity, Toast.LENGTH_SHORT); // инициализация
-        toast.setGravity(Gravity.TOP, 0,0); // задание позиции на экране (положение, смещение по оси Х, смещение по оси Y)
-        toast.show(); // демонстрация тоста на экране
+        Toast toast = Toast.makeText(this, R.string.pause_activity, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP, 0,0);
+        toast.show();
         super.onPause();
     }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt("key", count);
+    }
 
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        count = savedInstanceState.getInt("key");
+    }
 
 }
